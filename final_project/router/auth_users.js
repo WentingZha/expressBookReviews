512a443e3784f1296d5dbb099e9fe7aa6273b69b
ZHA Wen Ting <wentingzha881030@gmail.com>
2025-01-31 22:18:56 -0500

    Async

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const token = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
 let users = [
     {"username":"zhawenting","password":"123"}
 ];
+
+let bookList = [
+
+    {"author": "Chinua Achebe","title": "Things Fall Apart", "isbn":"1","reviews": {"date":"1/2/2025","reviewer":"zhawenting","commanded":"Chapter1"} },
+    {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
+    {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
+    {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
+    {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
+    {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
+    {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
+    {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
+    {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
+    {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+]
 
 const isValid = (username)=>{ //returns boolean
     let filtered_users = authenticatedUser.filter((user) => book.username === username);
@@ -21,15 +35,76 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const user = req.body.user;
+    if (!user) {
+        return res.status(404).json({ message: "Body Empty" });
+    }
+    // Generate JWT access token
+    let accessToken = token.sign({
+        data: user
+    }, 'access', { expiresIn: 60 * 60 });
+
+    // Store access token in session
+    req.session.authorization = {
+        accessToken
+    }
+    return res.status(200).send("User successfully logged in");
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  let filtered_books = bookList.filter((user) => user.isbn === isbn);
+  
+  if (filtered_books.length > 0) {
+      // Select the first matching user and update attributes if provided
+      var filtered_book = filtered_books[0];
+      let bookList1 = bookList.filter((book) => book.isbn != isbn);
+      filtered_book.reviews = {
+        "date":"2/2/2025",
+        "reviewer":"zhawenting",
+        "commanded":"Chapter2"}
+      bookList1.push(filtered_book);
+      
+      // Send success message indicating the user has been updated
+      res.send(bookList1);
+  } else {
+      // Send error message if no user found
+      res.send("Unable to find book!");
+  }
 });
+
+// Add a book review
+regd_users.put("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    let filtered_books = bookList.filter((user) => user.isbn === isbn);
+    
+    if (filtered_books.length > 0) {
+        // Select the first matching user and update attributes if provided
+        var filtered_book = filtered_books[0];
+        let bookList1 = bookList.filter((book) => book.isbn != isbn);
+        filtered_book.reviews = {
+          "date":"2/2/2025",
+          "reviewer":"zhawenting",
+          "commanded":"Chapter2"}
+        bookList1.push(filtered_book);
+        
+        // Send success message indicating the user has been updated
+        res.send(bookList1);
+    } else {
+        // Send error message if no user found
+        res.send("Unable to find book!");
+    }
+  });
+
+
+  regd_users.delete("auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    var bookList1 = bookList.filter((book) => book.isbn === isbn);
+    bookList1[0].reviews = "";
+    res.send(bookList1);
+});
+
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -19,49 +19,96 @@ let bookList = [
 ]
 
 public_users.post("/register", (req,res) => {
-    if (req.session.authorization) { // Get the authorization object stored in the session
-        token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
-        jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
-          if (!err) {
-            req.user = user;
-            next();
-          } else {
-            return res.status(403).json({ message: "User not authenticated" });
-          }
-        });
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) {
+        users.push({ "username": username, "password": password });
+        return res.status(200).json({ message: "User successfully registered. Now you can login" });
       } else {
-        return res.status(403).json({ message: "User not logged in" });
+        return res.status(404).json({ message: "User already exists!" });
       }
+    }
+    return res.status(404).json({ message: "Unable to register user." });
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({bookList}, null));
+    
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+        resolve("Promise resolved")
+        },2000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+        res.send(JSON.stringify({bookList}, null));
+    })
+    console.log("After calling promise");
+   
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-    const isbn = req.params.isbn;
-    let filtered_books = bookList.filter((book) => book.isbn === isbn);
-    res.send(filtered_books);
+
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+           
+        },2000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+        const isbn = req.params.isbn;
+        let filtered_books = bookList.filter((book) => book.isbn === isbn);
+        res.send(filtered_books);
+    })
+    console.log("After calling promise");
+   
+
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-    const author = req.params.author;
-    let filtered_books = bookList.filter((book) => book.author === author);
-    res.send(filtered_books);
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+          
+        },2000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+        const author = req.params.author;
+        let filtered_books = bookList.filter((book) => book.author === author);
+        res.send(filtered_books);
+    })
+    console.log("After calling promise");
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-    const title = req.params.title;
-    let filtered_books = bookList.filter((book) => book.title.includes(title));
-    res.send(filtered_books);
+    let myPromise = new Promise((resolve,reject) => {
+        setTimeout(() => {
+           
+        },2000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+         
+        const title = req.params.title;
+        let filtered_books = bookList.filter((book) => book.title.includes(title));
+        res.send(filtered_books);
+    })
+    console.log("After calling promise");
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
+    
     const isbn = req.params.isbn;
     console.log(isbn)
     let filtered_books = bookList.filter((book) => book.isbn === isbn);
