2a0dae431699c642964491355e6b24f378b574e9
ZHA Wen Ting <wentingzha881030@gmail.com>
2025-01-31 22:40:35 -0500

    Promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -52,17 +52,17 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-
+    var filtered_books;
     let myPromise = new Promise((resolve,reject) => {
+        const isbn = req.params.isbn;
+        filtered_books = bookList.filter((book) => book.isbn === isbn);
         setTimeout(() => {
-           
+            resolve("Promise resolved")
         },2000)})
 
     console.log("Before calling promise");
 
     myPromise.then((successMessage) => {
-        const isbn = req.params.isbn;
-        let filtered_books = bookList.filter((book) => book.isbn === isbn);
         res.send(filtered_books);
     })
     console.log("After calling promise");
@@ -72,16 +72,19 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
+    var filtered_books;
     let myPromise = new Promise((resolve,reject) => {
+
+        const author = req.params.author;
+        filtered_books = bookList.filter((book) => book.author === author);
         setTimeout(() => {
-          
+            resolve("Promise resolved")
         },2000)})
 
     console.log("Before calling promise");
 
     myPromise.then((successMessage) => {
-        const author = req.params.author;
-        let filtered_books = bookList.filter((book) => book.author === author);
+      
         res.send(filtered_books);
     })
     console.log("After calling promise");
@@ -90,17 +93,17 @@ public_users.get('/author/:author',function (req, res) {
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
+    var filtered_books;
     let myPromise = new Promise((resolve,reject) => {
+        const title = req.params.title;
+        filtered_books = bookList.filter((book) => book.title.includes(title));
         setTimeout(() => {
-           
+            resolve("Promise resolved")
         },2000)})
 
     console.log("Before calling promise");
 
     myPromise.then((successMessage) => {
-         
-        const title = req.params.title;
-        let filtered_books = bookList.filter((book) => book.title.includes(title));
         res.send(filtered_books);
     })
     console.log("After calling promise");
@@ -109,10 +112,22 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
     
-    const isbn = req.params.isbn;
-    console.log(isbn)
-    let filtered_books = bookList.filter((book) => book.isbn === isbn);
-    res.send(filtered_books[0].reviews);
+    var filtered_books;
+    let myPromise = new Promise((resolve,reject) => {
+        const isbn = req.params.isbn;
+        filtered_books = bookList.filter((book) => book.isbn === isbn);
+        setTimeout(() => {
+           
+        },2000)})
+
+    console.log("Before calling promise");
+
+    myPromise.then((successMessage) => {
+        res.send(filtered_books[0].reviews);
+    })
+    console.log("After calling promise");
+
+   
 });
 
 module.exports.general = public_users;
